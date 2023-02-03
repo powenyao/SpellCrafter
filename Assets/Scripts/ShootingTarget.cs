b6afc3746342ce3cf8b3b809272ec43fb62fed53
powenyao <powenyao@gmail.com>
2023-02-03 10:37:29 -0800

    1. Basic Target (Sphere) and Variant (Cube) i. Can be destroyed by spells ii. material change while hit for now 2. Commented out particle system code in SpellBaseVisualization that's not being usd atm to reduce flooding console

---
+++ Assets/Scripts/ShootingTarget.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ShootingTarget : MonoBehaviour, IDamageReceiver
+{
+    [SerializeField]
+    private float hp = 15;
+
+    [SerializeField]
+    private Renderer assignedRenderer;
+
+    [SerializeField]
+    private AudioClip AC_OnHit;
+
+    [SerializeField]
+    private Enum_Elements _currentElement = Enum_Elements.GrayNormal;
+
+    void Start()
+    {
+        ChangeElemental(_currentElement);
+    }
+    
+    void OnCollisionEnter(Collision other)
+    {
+        //Dev.Log("[ShootingTarget.cs] OnCollisionEnter");
+        if (other.gameObject.TryGetComponent<IDamageDealer>(out IDamageDealer dealer))
+        {
+            ReceiveDamage(dealer);
+            assignedRenderer.material.SetColor("_BaseColor", Color.red);
+            //Dev.Log("[ShootingTarget.cs] OnCollisionEnter > " + dealer.GetDamageType());            
+        }
+//        Dev.Log(other.gameObject.name);
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.TryGetComponent<IDamageDealer>(out IDamageDealer dealer))
+        {
+            assignedRenderer.material.SetColor("_BaseColor", Color.yellow);        
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        assignedRenderer.material.SetColor("_BaseColor", Color.white);
+        assignedRenderer.material.color = Core.Ins.UIEffectsManager.GetColorForElement(_currentElement);
+        //Dev.Log("sword on collision exit");
+    }
+
+    private void ShowDamage(IDamageDealer dealer)
+    {
+    }
+
+    private void Update()
+    {
+        // if (Input.GetKeyUp(KeyCode.I))
+        // {
+        //     ChangeElemental((Enum_Elements)(Random.value * 7));
+        // }
+    }
+
+    //@vinay might want to move this elsewhere if we aren't using a generic bonus
+    private int ElementalDamageMultiplier = 3;
+
+    
+    public void ReceiveDamage(IDamageDealer damageDealer)
+    {
+        var damageVal = (int)damageDealer.GetDamageValue();
+        var damageType = damageDealer.GetDamageType();
+        //Dev.Log("damageVal " + damageVal);
+        // Dev.Log("Damage Value: " + damageVal.ToString() + " Damage Type: " + dealer.GetDamageType());
+
+        if (damageVal > 0)
+        {
+            if (damageType == _currentElement && damageType != Enum_Elements.GrayNormal)
+            {
+                damageVal = damageVal * ElementalDamageMultiplier;
+            }
+            
+
+            //Visuals
+            var color = Core.Ins.UIEffectsManager.GetColorForElement(damageDealer.GetDamageType());
+            Core.Ins.UIEffectsManager.RequestPopUp(this.transform, damageVal.ToString(), color);
+
+            //Sound
+            //Core.Ins.AudioManager.PlayAudio(AC_OnHit, ENUM_Audio_Type.Sfx);
+
+            hp -= damageVal;
+
+            if (hp <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    
+
+    public void ChangeElemental(Enum_Elements newElement)
+    {
+        _currentElement = newElement;
+        assignedRenderer.material.color = Core.Ins.UIEffectsManager.GetColorForElement(_currentElement);
+    }
+
+    [ContextMenu("Debug_Receive Damage 10 Electro")]
+    public void Debug_ReceiveDamage()
+    {
+        DamageDealerForDebugging dealer = new DamageDealerForDebugging(10, Enum_Elements.PurpleElectro);
+        ReceiveDamage(dealer);
+    }
+}
--- Assets/Scripts/Sorcery/SpellBaseVisualization.cs
+++ Assets/Scripts/Sorcery/SpellBaseVisualization.cs
@@ -47,69 +47,69 @@ public class SpellBaseVisualization : MonoBehaviour
             Dev.LogWarning("Cannot find renderer to ChangeElement to " + elementType.ToString());
         }
 
-        if (_particleSystem)
-        {
-            var main = _particleSystem.main;
-            main.startColor = elementalColor;
-            //0, 0
-            //255, 15.5
-            //249, 90.9
-            //0, 100
-
-            //37.6
-            //52.8
-            //66.5
-            var time1 = 0.376f;
-            //var time2 = 0.528f;
-            var time3 = 0.665f;
-            Gradient grad = new Gradient();
-            grad.SetKeys(
-                new GradientColorKey[]
-                {
-                    new GradientColorKey(elementalColor, time1), /*new GradientColorKey(Color.white, time2),*/
-                    new GradientColorKey(elementalColor, time3)
-                },
-                new GradientAlphaKey[]
-                {
-                    new GradientAlphaKey(0.0f, 0.0f), new GradientAlphaKey(255.0f, 0.155f),
-                    new GradientAlphaKey(249.0f, 0.909f), new GradientAlphaKey(0f, 1.0f)
-                });
-
-            var col = _particleSystem.colorOverLifetime;
-            col.color = grad;
-        }
-        else
-        {
-            Dev.LogWarning("Cannot find _particleSystem to ChangeElement to " + elementType.ToString());
-        }
-
-        if (_particleSystem2)
-        {
-            var main = _particleSystem2.main;
-            main.startColor = elementalColor;
-            
-            var time1 = 0.376f;
-            //var time2 = 0.528f;
-            var time3 = 0.665f;
-            Gradient grad = new Gradient();
-            grad.SetKeys(
-                new GradientColorKey[]
-                {
-                    new GradientColorKey(elementalColor, time1), /*new GradientColorKey(Color.white, time2),*/
-                    new GradientColorKey(elementalColor, time3)
-                },
-                new GradientAlphaKey[]
-                {
-                    new GradientAlphaKey(0.0f, 0.0f), new GradientAlphaKey(255.0f, 0.155f),
-                    new GradientAlphaKey(249.0f, 0.909f), new GradientAlphaKey(0f, 1.0f)
-                });
-
-            var col = _particleSystem.colorOverLifetime;
-            col.color = grad;
-        }
-        else
-        {
-            Dev.LogWarning("Cannot find _particleSystem2 to ChangeElement to " + elementType.ToString());
-        }
+        // if (_particleSystem)
+        // {
+        //     var main = _particleSystem.main;
+        //     main.startColor = elementalColor;
+        //     //0, 0
+        //     //255, 15.5
+        //     //249, 90.9
+        //     //0, 100
+        //
+        //     //37.6
+        //     //52.8
+        //     //66.5
+        //     var time1 = 0.376f;
+        //     //var time2 = 0.528f;
+        //     var time3 = 0.665f;
+        //     Gradient grad = new Gradient();
+        //     grad.SetKeys(
+        //         new GradientColorKey[]
+        //         {
+        //             new GradientColorKey(elementalColor, time1), /*new GradientColorKey(Color.white, time2),*/
+        //             new GradientColorKey(elementalColor, time3)
+        //         },
+        //         new GradientAlphaKey[]
+        //         {
+        //             new GradientAlphaKey(0.0f, 0.0f), new GradientAlphaKey(255.0f, 0.155f),
+        //             new GradientAlphaKey(249.0f, 0.909f), new GradientAlphaKey(0f, 1.0f)
+        //         });
+        //
+        //     var col = _particleSystem.colorOverLifetime;
+        //     col.color = grad;
+        // }
+        // else
+        // {
+        //     Dev.LogWarning("Cannot find _particleSystem to ChangeElement to " + elementType.ToString());
+        // }
+        //
+        // if (_particleSystem2)
+        // {
+        //     var main = _particleSystem2.main;
+        //     main.startColor = elementalColor;
+        //     
+        //     var time1 = 0.376f;
+        //     //var time2 = 0.528f;
+        //     var time3 = 0.665f;
+        //     Gradient grad = new Gradient();
+        //     grad.SetKeys(
+        //         new GradientColorKey[]
+        //         {
+        //             new GradientColorKey(elementalColor, time1), /*new GradientColorKey(Color.white, time2),*/
+        //             new GradientColorKey(elementalColor, time3)
+        //         },
+        //         new GradientAlphaKey[]
+        //         {
+        //             new GradientAlphaKey(0.0f, 0.0f), new GradientAlphaKey(255.0f, 0.155f),
+        //             new GradientAlphaKey(249.0f, 0.909f), new GradientAlphaKey(0f, 1.0f)
+        //         });
+        //
+        //     var col = _particleSystem.colorOverLifetime;
+        //     col.color = grad;
+        // }
+        // else
+        // {
+        //     Dev.LogWarning("Cannot find _particleSystem2 to ChangeElement to " + elementType.ToString());
+        // }
     }
 }
