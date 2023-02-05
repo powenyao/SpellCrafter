d14be73d38649fb778ce0495aa8742a71e2c202a
Nicholas Owen Weber <noweber@gmail.com>
2023-02-05 12:53:36 -0800

    Track Current and Best Scores for Each Playthrough With End Screen UI Display
    This commit tracks the score sum for all levels in a given playthrough and displays their current and best in the end screen scene.

--- Assets/Scripts/Gameplay/EndScreen.cs
+++ Assets/Scripts/Gameplay/EndScreen.cs
@@ -3,19 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using Assets.Scripts.Gameplay;
 
 public class EndScreen : MonoBehaviour
 {
+    public TMP_Text LastScoreText;
+
+    public TMP_Text BestScoreText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetScoreString(ref LastScoreText, PlayerScores.LAST_SCORE_KEY, "Current Score");
+        SetScoreString(ref BestScoreText, PlayerScores.BEST_SCORE_KEY, "Best Score");
+    }
+
+    private void SetScoreString(ref TMP_Text text, string scoreKey, string scoreLabel)
+    {
+        if (text != null && !string.IsNullOrEmpty(scoreKey) && !string.IsNullOrEmpty(scoreLabel))
+        {
+            Score score = PlayerScores.Get(scoreKey);
+            if (score != null)
+            {
+                text.text = scoreLabel + ": " + score.manaCost.ToString();
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void ReplayGame()
