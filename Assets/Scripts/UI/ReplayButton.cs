3aa5fb2cfd55dfb3c3e5679e62db22064fc03a68
Vinay Gupta <vinay.23.gupta@outlook.com>
2023-02-05 16:30:16 -0800

    Critical bug fix in end screen

--- Assets/Scripts/UI/ReplayButton.cs
+++ Assets/Scripts/UI/ReplayButton.cs
@@ -14,4 +14,9 @@ public class ReplayButton : MonoBehaviour
     {
         gmRef.RestartLevel();
     }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
 }
